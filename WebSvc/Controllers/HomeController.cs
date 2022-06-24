f73682eed543545af087f3236d68b0a89522546c
Обухов Павел <pavelo@kommersant.ru>
2022-06-24 20:34:24 +0300

    init

---
+++ WebSvc/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System.Data.Common;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace SphinxQueryCore.Net.Controllers
+{
+	public class HomeController : Controller
+	{
+		public async Task<IActionResult> Index(string host, int? port, string statement)
+		{
+			var model = await LoadModel(host, (uint)(port ?? 0), statement);
+
+			return View(model);
+		}
+
+		private async Task<IndexModel> LoadModel(string host, uint port, string statement)
+		{
+			if (string.IsNullOrEmpty(host)) return new IndexModel();
+
+			try
+			{
+				var connStrBuilder = new MySqlConnectionStringBuilder();
+				connStrBuilder.Server = host;
+				connStrBuilder.Port = port;
+				connStrBuilder.CharacterSet = "utf8";
+				connStrBuilder.Pooling = false;
+				connStrBuilder.SslMode = MySqlSslMode.Disabled;
+
+				using (var conn = new MySqlConnection(connStrBuilder.ToString()))
+				{
+					try
+					{
+						await conn.OpenAsync();
+					}
+					catch
+					{
+						// workaround http://sphinxsearch.com/bugs/view.php?id=2196
+						// for version of Sphinx earlier than 2.2.9
+						if (conn.State != System.Data.ConnectionState.Open)
+						{
+							throw;
+						}
+					}
+
+					var m = new IndexModel
+					{
+						Indexes = await MakeQueryAsync("show tables", conn),
+					};
+
+					if (!string.IsNullOrEmpty(statement))
+					{
+						m.Rows = MakeQueryAsync(statement, conn).Result;
+						m.Meta = MakeQueryAsync("show meta", conn).Result;
+						m.Status = MakeQueryAsync("show status", conn).Result;
+					}
+
+					return m;
+				}
+			}
+			catch (Exception x)
+			{
+				return new IndexModel { Exception = x };
+			}
+		}
+
+		private async Task<QueryResult> MakeQueryAsync(string statement, MySqlConnection conn)
+		{
+			var cmd = new MySqlCommand(statement, conn);
+			using (var r = await cmd.ExecuteReaderAsync())
+			{
+				return new QueryResult
+				{
+					ColumnNames = Enumerable.Range(0, r.FieldCount)
+						.Select(i => r.GetName(i))
+						.ToArray(),
+					Values = ReadAllValues(r)
+						.ToArray(),
+				};
+			}
+		}
+
+		private IEnumerable<object[]> ReadAllValues(DbDataReader r)
+		{
+			var row = new List<object>();
+
+			while (r.Read())
+			{
+				row.Clear();
+
+				for (int i = 0; i < r.FieldCount; i++)
+				{
+					row.Add(r.GetValue(i));
+				}
+
+				yield return row.ToArray();
+			}
+		}
+	}
+}
---
+++ WebSvc/Models/IndexModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SphinxQueryCore.Net
+{
+	public struct IndexModel
+	{
+		public string Host;
+		public int? Port;
+		public string Statement;
+
+		public Exception? Exception;
+
+		public QueryResult Indexes;
+		public QueryResult Rows;
+		public QueryResult Meta;
+		public QueryResult Status;
+	}
+
+	public struct QueryResult
+	{
+		public string[] ColumnNames;
+		public object[][] Values;
+	}
+}
